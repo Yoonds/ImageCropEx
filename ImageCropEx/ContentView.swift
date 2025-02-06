75babafc45e31e37cf78c768183fd4c3630f15fb
Yoonds <wkzks123@anipen.com>
2025-02-06 13:10:38 +0900

    크롭기능 기본기능 작업

--- ImageCropEx/ContentView.swift
+++ ImageCropEx/ContentView.swift
@@ -2,23 +2,44 @@
 //  ContentView.swift
 //  ImageCropEx
 //
-//  Created by YoonDaesung on 2/6/25.
+//  Created by YoonDaesung on 2/5/25.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    private var image = UIImage(named: "sample")!
+    
+    @State var isShowCropView: Bool = false
+    @State var croppedImage: UIImage?
+    @State var cropArea: CGRect?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button(action: {
+                isShowCropView = true
+            }, label: {
+                if let croppedImage = croppedImage {
+                    Image(uiImage: croppedImage)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200)
+                } else {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300)
+                }
+            })
         }
-        .padding()
+        .fullScreenCover(isPresented: $isShowCropView) {
+            CropView(
+                image: image,
+                cropArea: $cropArea,
+                croppedImage: $croppedImage
+            )
+        }
     }
-}
-
-#Preview {
-    ContentView()
+    
 }
