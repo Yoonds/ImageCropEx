7151f448cbbfd163b64fccc3a9d71b0cdb73dca1
Yoonds <wkzks123@anipen.com>
2025-02-06 13:07:20 +0900

    초기

---
+++ ImageCropEx/ImageCropExApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCropExApp.swift
+//  ImageCropEx
+//
+//  Created by YoonDaesung on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageCropExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
